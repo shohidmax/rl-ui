a4951a9ba601b514e084f4048902cd121daed469
Rashedul Islam <rashedul.afl@gmail.com>
2025-12-05 22:22:41 +0000

    a bit more lower the banner image height (_for element div_)

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -17,7 +17,7 @@ export function Hero() {
       <Carousel>
         <CarouselContent>
           <CarouselItem>
-            <div className="relative h-[50vh] md:h-[70vh] w-full">
+            <div className="relative h-[40vh] md:h-[60vh] w-full">
               <Image
                 src={heroImage.imageUrl}
                 alt="Promotional banner"
